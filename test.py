3f0b909417f77a78c9012034166896e1a6833320
john-tran <johntran1@hotmail.se>
2023-01-20 11:40:59 +0100

    Notes from lecture 2

--- test.py
+++ test.py
@@ -8,11 +8,12 @@
 # d = {key: idx ** 2 for idx, key in enumerate(li)}
 # print(d)
 
-# a = 0b101001
-# b = 0b011100
+a = 0b101001
+b = 0b011100
 # & Operator returns bit where both have 1 in the same position
-# print(f'a = {a:d} = 0b{a :06b}')
-# print(a)
+print(f'a = {a:d} = 0b{a :06b}')
+print(f'{a = }')
+
 
 
 # Multiple function expansion:
@@ -44,8 +45,34 @@ def mySum(a, b, c):
 print(mySum(*x))
 
 # Lambda functions
-axpy_lambda = lambda a, x, y : a*x + y
+axpy_lambda = lambda a, x, y: a * x + y
 print(axpy_lambda(2, 3, 4))
 
+
 # Functions are objects:
-def add
+def add_me(a, b): return a + b
+
+
+def mul_me(a, b): return a * b
+
+
+def div_me(a, b): return a / b
+
+
+functions = [add_me, mul_me, div_me]
+
+for f in functions:
+    print(f(2, 3))
+
+# x += [4, 5]
+# For lists the appending with += rewrites the reference lists as well
+# To actually make a copy use : or .copy
+
+from scipy.optimize import root
+
+f = lambda x: x ** 2 - 1
+r = root(f, x0=0.1)
+print(r)
+print(r.x)
+
+# Do not import everything. Can cause errors and very hard to debug
